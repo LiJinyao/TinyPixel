2d4f8810ea721156c89b4ba58e17f1d372859897
LiJinyao <lijinyao@outlook.com>
2016-10-05 15:32:26 +0800

    shear image.  TODO: change size

--- src/actions/imageProcess/shear.js
+++ src/actions/imageProcess/shear.js
@@ -7,6 +7,29 @@ const DIRC = {
   VERTICAL: 'VERTICAL',
 }
 import { getPixelPosition, getCoordinate } from './util'
+
+function horizontal(dX, dY, offset) {
+  /**
+   * 垂直偏移
+   * v = x - offset * y
+   * w = y
+   */
+    const v = Math.round(dX - offset * dY)
+    const w = dY
+    return { v, w }
+}
+
+function vertical(dX, dY, offset) {
+  /**
+   * 水平偏移
+   * v = x
+   * w = y - offset * x
+   */
+    const v = dX
+    const w = Math.round(dY - offset * dX)
+    return { v, w }
+}
+
 export default function shear(imageData, {direction = DIRC.HORIZONTAL, offset = 0} = {}) {
   if(offset === 0) {
     return imageData
@@ -23,12 +46,23 @@ export default function shear(imageData, {direction = DIRC.HORIZONTAL, offset = 
   const dPosition = getPixelPosition(width, height)
   /**
    * 反向映射
-   * v = x - xOffset
-   * w = y - yOffset
+   * 垂直偏移
+   * v = x - offset * y
+   * w = y
+   * 水平偏移
+   * v = x
+   * w = y - offset * x
    */
+  let transtionFunc = null
+  switch (direction) {
+    case DIRC.HORIZONTAL:
+      transtionFunc = horizontal
+      break;
+    default:
+      transtionFunc = vertical
+  }
   for(const [dX, dY, dIndex] of dPosition()) {
-    const v = dX - xOffset
-    const w = dY - yOffset
+    const { v, w } = transtionFunc(dX, dY, offset)
     if (v < width && w < height && v >= 0 && w >= 0) {
       const index = oCoor(v, w)
       dData[dIndex] = data[index]         // red
